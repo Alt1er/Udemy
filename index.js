5759bac4362167251aeb37333eb73cdc84b69720
Yehor <evgwozdew@gmail.com>
2024-07-20 22:21:35 +0300

    first homework

---
+++ index.js
@@ -0,0 +1,21 @@
+// "use steict"
+// const numberOfFilms = prompt('Скільки фільмів ви вж переглянули?', ''),
+//       lastFilm = prompt("Останный фыльм який ви дивились?", ''),
+//       mark = prompt("Якою буде ваша оцінка цього фільму?", '');45
+
+
+// // 2
+// const personalMovieDB = {
+//     'count': numberOfFilms,
+//     'movies': {},
+//     'actors': {},
+//     'genres': [],
+//     'privat': true
+// }
+
+// console.log(personalMovieDB)
+
+// // 3
+
+
+// personalMovieDB.movies[lastFilm] = mark;
