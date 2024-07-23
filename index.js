caed8724d24adaf5b103fafc18a6074b436395be
Yehor <evgwozdew@gmail.com>
2024-07-23 16:32:42 +0300

    Practise2

--- index.js
+++ index.js
@@ -1,21 +1,39 @@
-// "use steict"
-// const numberOfFilms = prompt('Скільки фільмів ви вж переглянули?', ''),
-//       lastFilm = prompt("Останный фыльм який ви дивились?", ''),
-//       mark = prompt("Якою буде ваша оцінка цього фільму?", '');45
+"use steict"
 
 
-// // 2
-// const personalMovieDB = {
-//     'count': numberOfFilms,К
-//     'movies': {},
-//     'actors': {},
-//     'genres': [],
-//     'privat': true
-// }
+const numberOfFilms = prompt('Скільки фільмів ви вж переглянули?', '');
 
-// console.log(personalMovieDB)
-
-// // 3
+    
+const personalMovieDB = {
+    'count': numberOfFilms,
+    'movies': {},
+    'actors': {},
+    'genres': [],
+    'privat': true
+}
 
 
-// personalMovieDB.movies[lastFilm] = mark;
+for(let i = 0; i < 2; i++){
+    const lastFilm = prompt("Останный фыльм який ви дивились?", ''),
+          mark = prompt("Якою буде ваша оцінка цього фільму?", '');
+
+    if( lastFilm != null && mark != null && lastFilm != '' && mark != '' && lastFilm.length < 50){
+        personalMovieDB.movies[lastFilm] = mark;
+        console.log("done")
+    } else{
+        console.log('error')
+         i--;
+    }
+}
+
+console.log(personalMovieDB)
+
+if(personalMovieDB.count < 10){
+    console.log('Ви переглянули достатьо мало фільмів')
+}else if (personalMovieDB.count > 10 && personalMovieDB.count < 30){
+    console.log('Ви класичний глядач')
+}else{ 
+    console.log("Ви кіноман")
+}
+
+personalMovieDB.movies[lastFilm] = mark;
